0458eb61c1f5a54420320fe5097f5bd4927cf3b5
Фаль Татьяна <tatty.fal@gmail.com>
2026-06-13 09:45:22 +0600

    The project prepared for work

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Routes } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout/Layout'
+
+function App() {
+
+  return (
+    <Layout>
+      <Routes>
+        
+      </Routes>
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Colorie Tracker</a>
+  </div>
+</nav>
+  )
+}
+
+export default Header
